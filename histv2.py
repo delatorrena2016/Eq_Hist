ac5c46048803d04c72f135a386cff30a43cb54c8
Alvaro de la Torre <37124283+delatorrena2016@users.noreply.github.com>
2024-12-07 21:24:41 -0600

    Add files via upload

--- histv2.py
+++ histv2.py
@@ -18,25 +18,24 @@ def equalization(cdf, M, N):
 def exp_dist(l, l_, x):
     return l_ + np.exp(-l*x)
 
+def normal_dist(mu, sigma,x):
+    return ((1/np.sqrt(2*np.pi*sigma**2))*np.exp(-1*(x - mu)**2/(2*sigma**2)))
+
 # Cargamos la imagen como una matriz con los valores de la intensidad de cada pixel a escala de grises 
 imf = cv2.imread("mich.jpg")
 im = cv2.cvtColor(imf, cv2.COLOR_BGR2GRAY)
+
+
+################# EJERCICIO 1   
 # definimos el arreglo de los posibles valores de la intensidad
 int_v = np.asarray([i for i in range(256)])
 # Generamos histograma al sumar sobre el numero de valores True (esto, en orden del arreglo de valores)
 h = [ (im==v).sum() for v in int_v] 
-
 # Cálculo de la función de distribución acumulada o cdf a histograma original
 cdf, norm_cdf = cdff(h)
 # Aplicación de ecuación de ecualización de histograma
 eq_h = equalization(cdf, im.shape[0], im.shape[1]) 
-# Cálculo de la función de distribución acumulada o cdf a histograma ecualizado
- 
-#h_target = np.zeros(256)
-#h_target = exp_dist(0.035, 0.05, int_v)
-#cdf_target, norm_cdf_target = cdff(h_target)
-
-
+print(eq_h)
 # Aplicar transformación por ecualización
 n_im = eq_h[im]
 # Histograma de imagen ecualizada
@@ -44,8 +43,22 @@ trans_h = [ (n_im==v).sum() for v in int_v]
 # Cálculo de la función de distribución acumulada o cdf a nuevo histograma 
 trans_cdf, trans_norm_cdf = cdff(trans_h)
 
-#matched_src = np.interp(eq_h, trans_cdf, int_v)
-#mat_im = matched_src[im]
+################## EJERCICIO 2
+# Histograma de distribución exponencial
+h_target = exp_dist(0.035, 0.01, int_v)
+cdf_target, norm_cdf_target = cdff(h_target)
+eq_h_target = equalization(cdf_target, im.shape[0], im.shape[1])
+
+matched_src = np.interp(eq_h, eq_h_target, int_v)
+mat_im = matched_src[im]
+
+################ Second Dist.
+h_target_n = normal_dist(50.0, 20.0, int_v)
+cdf_target_n, norm_cdf_target_n = cdff(h_target_n)
+eq_h_target_n = equalization(cdf_target_n, im.shape[0], im.shape[1])
+
+matched_src_n = np.interp(norm_cdf, norm_cdf_target_n, int_v)
+mat_im_n = matched_src_n[im]
 
 fig1, axs = plt.subplots(2, 2, figsize=(6, 4))
 
@@ -59,6 +72,7 @@ axs[0,1].set_xlim([0,256])
 axs[0,1].set_xlabel('Intensity value')
 axs[0,1].set_ylabel('Frequency of intensities')
 axs[0,1].set_title('Initial histogram')
+axs[0,1].legend()
 
 axs[1,0].imshow(n_im,  cmap='gray')
 axs[1,0].axis("off")
@@ -70,19 +84,42 @@ axs[1,1].set_xlim([0,256])
 axs[1,1].set_xlabel('Intensity value')
 axs[1,1].set_ylabel('Frequency of intensities')
 axs[1,1].set_title('Equalized histogram')
+axs[1,1].legend()
+
+
+fig2, axs1 = plt.subplots(2, 2, figsize=(6, 4))
+
+axs1[0,0].imshow(mat_im,  cmap='gray')
+axs1[0,0].axis("off")
+axs1[0,0].title.set_text('OG Picture')
+
+axs1[0,1].bar(int_v, h_target, label='Histogram', alpha=0.5)
+axs1[0,1].plot(int_v, norm_cdf_target, label='Normalized CDF', color = 'red', alpha=0.3)
+axs1[0,1].set_xlim([0,256])
+axs1[0,1].set_xlabel('Intensity value')
+axs1[0,1].set_ylabel('Frequency of intensities')
+axs1[0,1].set_title('Initial histogram')
+axs1[0,1].legend()
+
+axs1[1,0].imshow(mat_im_n,  cmap='gray')
+axs1[1,0].axis("off")
+axs1[1,0].title.set_text('Equalized Picture')
+
+axs1[1,1].bar(int_v, h_target_n, label='Histogram', alpha=0.5)
+axs1[1,1].plot(int_v, norm_cdf_target_n, label='Normalized CDF', color = 'red', alpha=0.3)
+axs1[1,1].set_xlim([0,256])
+axs1[1,1].set_xlabel('Intensity value')
+axs1[1,1].set_ylabel('Frequency of intensities')
+axs1[1,1].set_title('Equalized histogram')
+axs1[1,1].legend()
+
+
+
+
+
+
+
+
+
 
 plt.show()
-
-#ax1[1].legend()
-#ax2[1].legend()
-
-#fig2, (ax3, ax4) =plt.subplots(2, 1, sharex = True, figsize=(6, 4))
-#ax3.bar(int_v,eq_h)
-#ax3.plot(int_v, norm_eq_cdf)
-#ax4.bar(int_v, trans_h)
-#ax4.plot(int_v, trans_norm_cdf)
-#ax3.set_ylabel('Frequency of values')
-#ax4.set_ylabel('Cumulative frequency')
-#ax4.set_xlabel('Intensity value')
-#ax3.set_title('Equalized histogram')
-
